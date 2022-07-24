ab856a861e90cc218ebbd117bc84c4db8c8eda56
Nilton Morais <niltonmorais.code@gmail.com>
2022-07-24 20:53:33 -0300

    implementando cupom de desconto no PlaceOrder

--- src/application/PlaceOrder.ts
+++ src/application/PlaceOrder.ts
@@ -1,11 +1,13 @@
 import Order from "../domain/entity/Order";
+import CouponRepository from "../domain/repository/CouponRepository";
 import ItemRepository from "../domain/repository/ItemRepository";
 import OrderRepository from "../domain/repository/OrderRepository";
 
 export default class PlaceOrder {
     constructor(
         readonly itemRepository: ItemRepository,
-        readonly orderRepository: OrderRepository
+        readonly orderRepository: OrderRepository,
+        readonly couponRepository: CouponRepository
     ) {}
 
     async execute(input: Input): Promise<Output> {
@@ -14,6 +16,10 @@ export default class PlaceOrder {
         for (const orderItem of input.orderItems) {
             const item = await this.itemRepository.get(orderItem.idItem);
             order.addItem(item, orderItem.quantity);
+        }
+        if (input.coupon) {
+            const coupon = await this.couponRepository.get(input.coupon);
+            order.addCoupon(coupon);
         }
         await this.orderRepository.save(order);
         const total = order.getTotal();
--- test/integration/application/PlaceOrder.test.ts
+++ test/integration/application/PlaceOrder.test.ts
@@ -3,6 +3,8 @@ import OrderRepositoryMemory from "../../../src/infra/repository/memory/OrderRep
 import PlaceOrder from "../../../src/application/PlaceOrder";
 import Dimension from "../../../src/domain/entity/Dimension";
 import Item from "../../../src/domain/entity/Item";
+import CouponRepositoryMemory from "../../../src/infra/repository/memory/CouponRepositoryMemory";
+import Coupon from "../../../src/domain/entity/Coupon";
 
 test("Deve fazer um pedido", async function () {
     const itemRepository = new ItemRepositoryMemory();
@@ -15,7 +17,12 @@ test("Deve fazer um pedido", async function () {
     itemRepository.save(new Item(3, "Cabo", 30, new Dimension(10, 10, 10), 1));
 
     const orderRepository = new OrderRepositoryMemory();
-    const placeOrder = new PlaceOrder(itemRepository, orderRepository);
+    const couponRepository = new CouponRepositoryMemory();
+    const placeOrder = new PlaceOrder(
+        itemRepository,
+        orderRepository,
+        couponRepository
+    );
     const input = {
         cpf: "935.411.347-80",
         orderItems: [
@@ -39,7 +46,12 @@ test("Deve fazer um pedido e gerar o código do pedido", async function () {
     itemRepository.save(new Item(3, "Cabo", 30, new Dimension(10, 10, 10), 1));
 
     const orderRepository = new OrderRepositoryMemory();
-    const placeOrder = new PlaceOrder(itemRepository, orderRepository);
+    const couponRepository = new CouponRepositoryMemory();
+    const placeOrder = new PlaceOrder(
+        itemRepository,
+        orderRepository,
+        couponRepository
+    );
     const input = {
         cpf: "935.411.347-80",
         orderItems: [
@@ -51,4 +63,34 @@ test("Deve fazer um pedido e gerar o código do pedido", async function () {
     };
     const output = await placeOrder.execute(input);
     expect(output.code).toBe("202100000001");
+});
+
+test("Deve fazer um pedido com desconto", async function () {
+    const itemRepository = new ItemRepositoryMemory();
+    itemRepository.save(
+        new Item(1, "Guitarra", 1000, new Dimension(100, 30, 10), 3)
+    );
+    itemRepository.save(
+        new Item(2, "Amplificador", 5000, new Dimension(50, 50, 50), 20)
+    );
+    itemRepository.save(new Item(3, "Cabo", 30, new Dimension(10, 10, 10), 1));
+    const orderRepository = new OrderRepositoryMemory();
+    const couponRepository = new CouponRepositoryMemory();
+    couponRepository.save(new Coupon("VALE20", 20));
+    const placeOrder = new PlaceOrder(
+        itemRepository,
+        orderRepository,
+        couponRepository
+    );
+    const input = {
+        cpf: "935.411.347-80",
+        orderItems: [
+            { idItem: 1, quantity: 1 },
+            { idItem: 2, quantity: 1 },
+            { idItem: 3, quantity: 3 },
+        ],
+        coupon: "VALE20",
+    };
+    const output = await placeOrder.execute(input);
+    expect(output.total).toBe(5132);
 });
