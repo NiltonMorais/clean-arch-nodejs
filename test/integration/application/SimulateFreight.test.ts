8b046915b839dff6b7fc3b39e02dc69bb2165592
Nilton Morais <niltonmorais.code@gmail.com>
2022-07-24 21:14:36 -0300

    caso de uso de cálculo de frete

---
+++ test/integration/application/SimulateFreight.test.ts
@@ -0,0 +1,27 @@
+import Dimension from "../../../src/domain/entity/Dimension";
+import Item from "../../../src/domain/entity/Item";
+import ItemRepositoryMemory from "../../../src/infra/repository/memory/ItemRepositoryMemory";
+import SimulateFreight from "../../../src/application/SimulateFreight";
+
+test("Deve simular o frete do pedido", async function () {
+    const itemRepository = new ItemRepositoryMemory();
+    itemRepository.save(
+        new Item(1, "Guitarra", 1000, new Dimension(100, 30, 10), 3)
+    );
+    itemRepository.save(
+        new Item(2, "Amplificador", 5000, new Dimension(50, 50, 50), 20)
+    );
+    itemRepository.save(new Item(3, "Cabo", 30, new Dimension(10, 10, 10), 1));
+
+    const simulateFreight = new SimulateFreight(itemRepository);
+    const input = {
+        zipcode: "11111-11",
+        orderItems: [
+            { idItem: 1, quantity: 1 },
+            { idItem: 2, quantity: 1 },
+            { idItem: 3, quantity: 3 },
+        ],
+    };
+    const output = await simulateFreight.execute(input);
+    expect(output.total).toBe(260);
+});
